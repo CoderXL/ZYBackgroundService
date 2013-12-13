3f89a55e54f2cd81716379c2950c38a7f1d32f76
ABeen <abeen_8298@msn.com>
2013-12-13 13:51:30 +0800

    Initial Commit

---
+++ ZYBackgroundService/ZYBackgroundServiceAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZYBackgroundServiceAppDelegate.h
+//  ZYBackgroundService
+//
+//  Created by ZYVincent on 13-12-13.
+//  Copyright (c) 2013年 ZYProSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYBackgroundServiceAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
